c91f219a120888c6051486cdab719dc3938d552a
Sneha Ghosh (Technical_Isopod_075) <153223798+SnehaghoshBarsha444@users.noreply.github.com>
2025-02-15 16:06:32 +0530

    Update Projects.jsx

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -6,10 +6,10 @@ import ProjectCard from "../cards/ProjectCard";
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  justify-contnet: center;
+  justify-content: center;
   margin-top: 50px;
   padding: 0px 16px;
-  position: rlative;
+  position: relative;
   z-index: 1;
   align-items: center;
 `;
@@ -27,6 +27,7 @@ const Wrapper = styled.div`
     flex-direction: column;
   }
 `;
+
 const Title = styled.div`
   font-size: 52px;
   text-align: center;
@@ -38,6 +39,7 @@ const Title = styled.div`
     font-size: 32px;
   }
 `;
+
 const Desc = styled.div`
   font-size: 18px;
   text-align: center;
@@ -54,12 +56,13 @@ const ToggleButtonGroup = styled.div`
   color: ${({ theme }) => theme.primary};
   font-size: 16px;
   border-radius: 12px;
-font-weight 500;
-margin: 22px 0;
-@media (max-width: 768px){
+  font-weight: 500;
+  margin: 22px 0;
+  @media (max-width: 768px) {
     font-size: 12px;
-}
+  }
 `;
+
 const ToggleButton = styled.div`
   padding: 8px 18px;
   border-radius: 6px;
@@ -77,6 +80,7 @@ const ToggleButton = styled.div`
   background:  ${theme.primary + 20};
   `}
 `;
+
 const Divider = styled.div`
   width: 1.5px;
   background: ${({ theme }) => theme.primary};
@@ -92,57 +96,47 @@ const CardContainer = styled.div`
 
 const Projects = () => {
   const [toggle, setToggle] = useState("all");
+
   return (
     <Container id="Projects">
       <Wrapper>
         <Title>Projects</Title>
-        <Desc
-          style={{
-            marginBottom: "40px",
-          }}
-        >
-          I have worked on a wide range of projects. From web apps to android
-          apps. Here are some of my projects.
+        <Desc style={{ marginBottom: "40px" }}>
+          I have worked on a wide range of projects, from web apps to Android apps and even game development. Here are some of my projects.
         </Desc>
 
         <ToggleButtonGroup>
-          <ToggleButton
-            active={toggle === "all"}
-            onClick={() => setToggle("all")}
-          >
+          <ToggleButton active={toggle === "all"} onClick={() => setToggle("all")}>
             ALL
           </ToggleButton>
           <Divider />
-          <ToggleButton
-            active={toggle === "web app"}
-            onClick={() => setToggle("web app")}
-          >
-            WEB APP"S
+          <ToggleButton active={toggle === "web app"} onClick={() => setToggle("web app")}>
+            WEB APPS
           </ToggleButton>
           <Divider />
-          <ToggleButton
-            active={toggle === "android app"}
-            onClick={() => setToggle("android app")}
-          >
-            ANDROID APP'S
+          <ToggleButton active={toggle === "android app"} onClick={() => setToggle("android app")}>
+            ANDROID APPS
           </ToggleButton>
           <Divider />
-          <ToggleButton
-            active={toggle === "machine learning"}
-            onClick={() => setToggle("machine learning")}
-          >
+          <ToggleButton active={toggle === "machine learning"} onClick={() => setToggle("machine learning")}>
             MACHINE LEARNING
+          </ToggleButton>
+          <Divider />
+          <ToggleButton active={toggle === "game development"} onClick={() => setToggle("game development")}>
+            GAME DEVELOPMENT
+          </ToggleButton>
+          <Divider />
+          <ToggleButton active={toggle === "hackathon"} onClick={() => setToggle("hackathon")}>
+            HACKATHONS
           </ToggleButton>
         </ToggleButtonGroup>
 
         <CardContainer>
-          {toggle === "all" &&
-            projects.map((project) => <ProjectCard project={project} />)}
-          {projects
-            .filter((item) => item.category === toggle)
-            .map((project) => (
-              <ProjectCard project={project} />
-            ))}
+          {toggle === "all"
+            ? projects.map((project) => <ProjectCard key={project.id} project={project} />)
+            : projects
+                .filter((item) => item.category === toggle)
+                .map((project) => <ProjectCard key={project.id} project={project} />)}
         </CardContainer>
       </Wrapper>
     </Container>
